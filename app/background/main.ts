6f51f2b9d0be2371b3caad88cdd906ca1c19e2f9
Nick Kozlov <enormousrage@gmail.com>
2019-08-05 16:36:58 +0300

    artem code to new branch

--- app/background/main.ts
+++ app/background/main.ts
@@ -1,35 +1,132 @@
 import * as passworder from 'browser-passworder';
-import { timer } from "rxjs";
+import { from, Observable, of, Subject, timer } from "rxjs";
+import { environment } from "../view/src/environments/environment";
+import { IStorageData } from "../view/src/app/services/storage.service";
+import { map, switchMap, switchMapTo, tap, timeout } from "rxjs/operators";
 
-async function f() {
+// const STORAGE_KEY = 'all';
+//
+// function getFromStorageRaw(): Promise<string> {
+//     return new Promise<any>((resolve, reject) => {
+//         if (environment.production) {
+//             chrome.storage.local.get(STORAGE_KEY, (result) => resolve(result[STORAGE_KEY]));
+//         } else {
+//             const result = localStorage.getItem(STORAGE_KEY);
+//             resolve(result);
+//         }
+//     });
+// }
+//
+// function getFromStorage(password: string): Observable<IStorageData> {
+//     return from(this.getFromStorageRaw()).pipe(
+//         switchMap((encrypted: any) => {
+//             return from(passworder.decrypt(password, encrypted));
+//         }),
+//         map((dectypted: any) => {
+//             return dectypted as IStorageData;
+//         })
+//     );
+// }
+
+interface MessageFromPage {
+    type: 'startSession' | 'dropSession' | 'keepAlive' | 'restoreSession';
+    password?: string;
+    timeout?: number;
+}
 
 
-    chrome.runtime.onConnect.addListener(function (port) {
-        console.log("Connected .....");
-        port.onMessage.addListener(function (msg) {
-            console.log("message recieved" + msg);
-            port.postMessage("Hi Popup.js");
+// msg.password
+// const decryptedDate$ = passworder.decrypt(password, data);
+// of(decryptedDate$).pipe(
+//     tap(() => {
+//         password = msg.password
+//     }),
+//     switchMap(tm$),
+//     // switchMap((decoded) => {
+//     //     password = msg.password;
+//     // }),
+// ).subscribe(
+//     () => {
+//     },
+//     () => {
+//         password = '';
+//     }
+// );
+// try to decode and save password
+
+class MessageProcessor {
+    private password = '';
+    private keepAlive$ = new Subject<boolean>();
+
+    processMsg(msg: MessageFromPage) {
+        if (msg.type === 'startSession') {
+
+            this.password = msg.password;
+            // const timeout$ = timeout(msg.timeout);
+
+            this.keepAlive$.pipe(
+                tap(() => {
+                    console.log('keep alive');
+                }),
+                switchMap((x: any) => {
+                    console.log('keep alive, do switch map, and attach timeout');
+                    return timeout(msg.timeout);
+                }),
+            ).subscribe(
+                () => {
+                    // Normal keep alive comes here
+                    console.log('keep alive, do nothing');
+                },
+                () => {
+                    console.log('log out on timeout');
+                    this.password = '';
+                })
+        }
+
+        if (msg.type === 'dropSession') {
+            this.password = '';
+        }
+
+        if (msg.type === 'keepAlive') {
+            this.keepAlive$.next(true);
+        }
+
+        if (msg.type === 'restoreSession') {
+            chrome.runtime.sendMessage({password: this.password});
+        }
+    }
+}
+
+
+function f() {
+
+
+    chrome.runtime.onConnect.addListener((port) => {
+
+        const processor = new MessageProcessor();
+
+        port.onMessage.addListener((msg: MessageFromPage) => {
+            if (msg || !msg.type) {
+                return;
+            }
+            processor.processMsg(msg)
         });
     });
 
 
-    const data = {a: 1};
+    // timer(0, 2000).subscribe(() => {
+    //     console.log('1123');
+    // });
 
-    const password = '11';
-
-    timer(0, 2000).subscribe(() => {
-        console.log('1123');
-    });
-
-    const encrypted = await passworder.encrypt(password, data);
-    console.log(encrypted);
-
-    let a = "https://dex.binance.org/api/v1/account/bnb1hgm0p7khfk85zpz5v0j8wnej3a90w709vhkdfu";
-    const x = fetch(a).then((x) => {
-        x.json().then((obj) => {
-            debugger
-        })
-    })
+    // const encrypted = await passworder.encrypt(password, data);
+    // console.log(encrypted);
+    //
+    // let a = "https://dex.binance.org/api/v1/account/bnb1hgm0p7khfk85zpz5v0j8wnej3a90w709vhkdfu";
+    // const x = fetch(a).then((x) => {
+    //     x.json().then((obj) => {
+    //         debugger
+    //     })
+    // })
 }
 
 f();
--- app/view/src/app/services/auth.service.ts
+++ app/view/src/app/services/auth.service.ts
@@ -1,15 +1,23 @@
-import {Injectable} from '@angular/core';
-import {Observable, of, Subject} from 'rxjs';
-import {catchError, map, startWith} from 'rxjs/operators';
-import {IStorageData, StorageService} from './storage.service';
-import {StateService} from './state.service';
-import {Router} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable, of, Subject } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { IStorageData, StorageService } from './storage.service';
+import { StateService } from './state.service';
+import { Router } from '@angular/router';
+
+
+interface MessageFromPage {
+    type: 'startSession' | 'dropSession' | 'keepAlive' | 'restoreSession';
+    password?: string;
+    timeout?: number;
+}
+
 
 @Injectable()
 export class AuthService {
 
     private subject$ = new Subject<boolean>();
-    private _isLoggedIn: boolean = false;
+    private _isLoggedIn = false;
 
     get isLoggedIn(): boolean {
         return this._isLoggedIn;
@@ -25,8 +33,23 @@ export class AuthService {
     // subscription: Subscription;
 
     constructor(private storage: StorageService, private router: Router, private stateService: StateService) {
+
+
         this.isLoggedIn$ = this.subject$.asObservable().pipe(
             startWith(this.isLoggedIn)
+        );
+
+        chrome.runtime.sendMessage(
+            {type: 'restoreSession'},
+            (response) => {
+                if (response.password) {
+                    this.login(response.password).subscribe(
+                        () => {
+                            this.router.navigate(['/main']);
+                        }
+                    );
+                }
+            }
         );
     }
 
@@ -36,6 +59,19 @@ export class AuthService {
             map((data: IStorageData) => {
                 this.isLoggedIn = true;
                 this.stateService.initState(data, password);
+
+
+                // init session with a live time 10 sec
+                chrome.runtime.sendMessage(
+                    {type: 'startSession', timeout: 10000},
+                    (response) => {
+                        if (response.password) {
+                            this.login(response.password); // TODO: Also send keep a lives just while app is running
+                        }
+                    }
+                );
+
+
                 return true;
             }),
             catchError((error) => {
@@ -44,10 +80,21 @@ export class AuthService {
         );
     }
 
+
     logout(): void {
+
+        chrome.runtime.sendMessage(
+            {type: 'dropSession'},
+            (response) => {
+                if (response.password) {
+                    this.login(response.password);
+                }
+            }
+        );
+
         this.isLoggedIn = false;
         this.router.navigate(['/unlock']).then(() => {
             this.stateService.resetState();
-        })
+        });
     }
 }
